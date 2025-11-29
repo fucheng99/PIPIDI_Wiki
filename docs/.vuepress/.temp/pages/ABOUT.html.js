50fe650f29ef5b8108c9e94e169ee3070796b1d1
fucheng99 <461926138@qq.com>
2025-11-29 19:23:18 +0800

    update web ui

--- docs/.vuepress/.temp/pages/ABOUT.html.js
+++ docs/.vuepress/.temp/pages/ABOUT.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/46192/Desktop/Wiki/repo/docs/.vuepress/.temp/pages/ABOUT.html.vue"
-const data = JSON.parse("{\"path\":\"/ABOUT.html\",\"title\":\"关于\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"ABOUT.md\"}")
+const data = JSON.parse("{\"path\":\"/ABOUT.html\",\"title\":\"关于\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1764414577000,\"contributors\":[{\"name\":\"fucheng99\",\"username\":\"fucheng99\",\"email\":\"461926138@qq.com\",\"commits\":1,\"url\":\"https://github.com/fucheng99\"}],\"changelog\":[{\"hash\":\"1ff03227888b46a54d9e1d32cea43cb937627552\",\"time\":1764414577000,\"email\":\"461926138@qq.com\",\"author\":\"fucheng99\",\"message\":\"update the web ui\"}]},\"filePathRelative\":\"ABOUT.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
